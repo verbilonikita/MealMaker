4959dd7bca4907bcccb6d185677e101d7b0464b6
verbilonikita <nikita@MacBook-Pro-Nikita.local>
2021-01-28 17:07:31 +0000

    InitialArchitecture

--- src/js/controller.js
+++ src/js/controller.js
@@ -11,3 +11,118 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+const renderSpinner = function (parentEl) {
+  const markup = `
+ <div class="spinner">
+          <svg>
+            <use href="src/img/icons.svg#icon-loader"></use>
+          </svg>
+  </div>
+  `;
+  recipeContainer.innerHTML = '';
+  parentEl.insertAdjacentHTML('afterbegin', markup);
+};
+
+const showRecipe = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+    if (!id) return;
+
+    //1. Loading Recipies
+    renderSpinner(recipeContainer);
+
+    //prettier-ignore
+    const res = await fetch(`https://forkify-api.herokuapp.com/api/get?rId=${id}`);
+    const data = await res.json();
+    //prettier-ignore
+    if (!res.ok) throw new Error(`${data.error.slice(0, 36)} Error ${res.status}.`);
+    let { recipe } = data;
+    console.log(recipe);
+
+    //2. Rendering Recipe
+
+    const markup = `
+    <figure class="recipe__fig">
+    <img src='${recipe.image_url}' alt="Tomato" class="recipe__img" />
+    <h1 class="recipe__title">
+      <span>${recipe.title}</span>
+    </h1>
+    </figure>
+    <div class="recipe__details">
+    <div class="recipe__info">
+      <svg class="recipe__info-icon">
+        <use href="src/img/icons.svg#icon-clock"></use>
+      </svg>
+      <span class="recipe__info-data recipe__info-data--minutes">60 </span>
+      <span class="recipe__info-text">minutes</span>
+    </div>
+    <div class="recipe__info">
+      <svg class="recipe__info-icon">
+        <use href="src/img/icons.svg#icon-users"></use>
+      </svg>
+      <span class="recipe__info-data recipe__info-data--people">4</span>
+      <span class="recipe__info-text">servings</span>
+    </div>
+
+    <div class="recipe__user-generated">
+      <svg>
+        <use href="src/img/icons.svg#icon-user"></use>
+      </svg>
+    </div>
+    <button class="btn--round">
+      <svg class="">
+        <use href="src/img/icons.svg#icon-bookmark-fill"></use>
+      </svg>
+    </button>
+    </div>
+
+    <div class="recipe__ingredients">
+    <h2 class="heading--2">Recipe ingredients</h2>
+    <ul class="recipe__ingredient-list">
+    ${recipe.ingredients
+      .map(ing => {
+        console.log(ing);
+        return `
+      <li class="recipe__ingredient">
+        <svg class="recipe__icon">
+          <use href="src/img/icons.svg#icon-check"></use>
+        </svg>
+        <div class="recipe__description">
+        ${ing}
+        </div>
+      </li>
+      `;
+      })
+      .join(' ')}
+    </ul>
+    </div>
+
+    <div class="recipe__directions">
+    <h2 class="heading--2">How to cook it</h2>
+    <p class="recipe__directions-text">
+      This recipe was carefully designed and tested by
+      <span class="recipe__publisher">${
+        recipe.publisher
+      }</span>. Please check out
+      directions at their website.
+    </p>
+    <a
+      class="btn--small recipe__btn"
+      href="${recipe.source_url}"
+      target="_blank"
+    >
+      <span>Directions</span>
+      <svg class="search__icon">
+        <use href="src/img/icons.svg#icon-arrow-right"></use>
+      </svg>
+    </a>
+    </div>`;
+    recipeContainer.innerHTML = '';
+    recipeContainer.insertAdjacentHTML('afterbegin', markup);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+['hashchange', 'load'].forEach(ev => window.addEventListener(ev, showRecipe));
